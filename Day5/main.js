086beab655e004b0a7e9fbedb8ebc51571253289
manhmoimoi05 <manhphanvp112@gmail.com>
2025-11-18 15:39:06 +0700

    Update main.js

--- Day5/main.js
+++ Day5/main.js
@@ -1,5 +1,7 @@
 const button = document.querySelector(".icon__search");
-
+// thêm sự kiện khi click vào khối icon
 button.addEventListener("click", function () {
+  // tác động phần tử cha của khối icon là khối search thêm class active
+  // dùng phương thức toggle khi có class này sẽ xóa còn chưa có sẽ thêm 
   this.parentElement.classList.toggle("active");
 });
